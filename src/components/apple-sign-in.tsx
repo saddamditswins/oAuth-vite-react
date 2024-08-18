259c01b11d277579b4f9f671b3502989ffc27dad
saddamditswins <78360679+saddamditswins@users.noreply.github.com>
2024-08-19 01:36:38 +0530

    updated apple signin implementatinon

--- src/components/apple-sign-in.tsx
+++ src/components/apple-sign-in.tsx
@@ -4,41 +4,35 @@ import { AppConstants, setLS } from "@/lib/utils";
 
 import { BsApple } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
-import { appleAuthHelpers } from "react-apple-signin-auth";
+import AppleSignInButton from "react-apple-signin-auth";
 import { AppleAuthResponse } from "@/types/auth";
 
 export default function AppleSignIn() {
   const navigate = useNavigate();
 
-  const login = async () => {
-    await appleAuthHelpers.signIn({
-      authOptions: {
+  return (
+    <AppleSignInButton
+      authOptions={{
         clientId: `${ENV_VAR.APPLE_CLIENT_ID}`,
-        scope: "email name",
         redirectURI: `${ENV_VAR.APP_DOMAIN}`,
-        state: "state",
+        scope: "email name",
         nonce: "nonce",
+        state: "state",
         usePopup: true,
-      },
-      onSuccess: (response: AppleAuthResponse) => {
+      }}
+      onSuccess={(response: AppleAuthResponse) => {
         console.log(response);
 
         setLS(AppConstants.auth_token, JSON.stringify(response));
         navigate(AppRoutes.profile);
-      },
-      onError: (error: any) => console.log(error),
-    });
-  };
-
-  return (
-    <button
-      onClick={login}
-      className="w-full border p-2 flex justify-center items-center gap-2 hover:bg-gray-50"
-    >
-      <BsApple className="h-6 w-6" />
-      <span className="text-base text-gray-700 font-medium">
-        Continue With Apple
-      </span>
-    </button>
+      }}
+      onError={(error: any) => console.error(error)}
+      skipScript={false}
+      render={(props: any) => <button {...props} className="w-full border p-2 flex justify-center items-center gap-2 hover:bg-gray-50">
+        <BsApple className="h-6 w-6" />
+        <span className="text-base text-gray-700 font-medium">
+          Continue With Apple
+        </span>
+      </button>} uiType={"light"}  />
   );
 }
