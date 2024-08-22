b75784e206ff635cd0faf63d63353914854e0462
saddamdits <saddamditswins@gmail.com>
2024-08-22 15:38:42 +0530

    fixed build issue

--- src/repo/auth.ts
+++ src/repo/auth.ts
@@ -1,5 +1,5 @@
 import apiHelper, { END_POINTS } from "@/lib/api-helper";
-import { AppConstants, getLS, removeFromLS } from "@/lib/utils";
+import { AppConstants, getLS } from "@/lib/utils";
 import { APIResponse } from "@/types/app";
 import { IAuthToken, ISignIn, ISignInResponse } from "@/types/auth";
 
